dbfb64c7771b0d1416a3a2df57ecb20b27ae1db9
LuLuLaLaLa <haesung0728@gmail.com>
2023-09-20 22:30:03 +0900

    [service] 차량 번호 응답 endpoint 구성

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, JSONResponse
+import text_extract_car_plate as tecp
 
 app = FastAPI()
 
@@ -18,51 +19,59 @@ async def say_hello(name: str):
 async def file_test():
     html_content = """
     <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <input id="imageFile" type="file" accept="image/*">
-    <button id="sendBtn" onclick="">파일 전송</button>
-</body>
-<script>
-    let file;
-
-    const $imageFile = document.querySelector("#imageFile");
-    const $sendBtn = document.querySelector("#sendBtn");
-
-    $imageFile.addEventListener("change", (e) => {
-        file = e.target.files[0];
-        console.log(file);
-    });
-
-    $sendBtn.addEventListener("click", () => {
-
-        let fd = new FormData();
-        fd.append("image", file);
-
-        fetch("/image/car-number", {
-            method: "POST",
-            body: fd
-        }).then((res) => {
-            return res.json();
-        }).then((res) => {
-            console.log(res);
-        }).catch(()=> {
-            alert("실패");
-        })
-    });
-
-</script>
-</html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <input id="imageFile" type="file" accept="image/*">
+            <button id="sendBtn" onclick="">파일 전송</button>
+        </body>
+        <script>
+            let file;
+        
+            const $imageFile = document.querySelector("#imageFile");
+            const $sendBtn = document.querySelector("#sendBtn");
+        
+            $imageFile.addEventListener("change", (e) => {
+                file = e.target.files[0];
+                console.log(file);
+            });
+        
+            $sendBtn.addEventListener("click", () => {
+        
+                let fd = new FormData();
+                fd.append("image", file);
+        
+                fetch("/image/car-number", {
+                    method: "POST",
+                    body: fd
+                }).then((res) => {
+                    return res.json();
+                }).then((res) => {
+                    console.log(res);
+                }).catch(()=> {
+                    alert("실패");
+                })
+            });
+        
+        </script>
+    </html>
     """
-
     return HTMLResponse(content=html_content)
 
 
 @app.post("/image/car-number")
 async def extract_car_number_from_image(image: UploadFile):
-    return {"filename": image.filename, "content_type": image.content_type}
+    extracted_car_plates = await tecp.extract_car_plates_from_image(image)
+
+    result = {
+        "carPlateCandidates": extracted_car_plates
+    }
+
+    return JSONResponse(
+        content=result,
+        status_code=200
+    )
